23bb6daf0706c337b737de7b9925805c46f1302f
Almira Khafizova <mimirka80@gmail.com>
2026-06-20 00:20:38 +0200

    Feat: update UI add background and cornerRadius

--- BarcodeScanner/Screens/BarcodeScanner/BarcodeScannerView.swift
+++ BarcodeScanner/Screens/BarcodeScanner/BarcodeScannerView.swift
@@ -11,11 +11,32 @@ struct BarcodeScannerView: View {
   
   @StateObject var viewModel = BarcodeScannerViewModel()
   
-    var body: some View {
-      NavigationView {
+  var body: some View {
+    NavigationView {
+      ZStack {
+        LinearGradient(
+          colors: [
+            Color.blue.opacity(0.20),
+            Color.purple.opacity(0.15),
+            Color.indigo.opacity(0.10),
+            Color.black.opacity(0.02)
+          ],
+          startPoint: .topLeading,
+          endPoint: .bottomTrailing
+        )
+        .ignoresSafeArea()
+        
         VStack {
-          ScannerView(scannedCode: $viewModel.scannedCode, alertItem: $viewModel.alertItem)
-            .frame(maxWidth: .infinity, maxHeight: 300)
+          ScannerView(scannedCode: $viewModel.scannedCode,
+                      alertItem: $viewModel.alertItem)
+          .frame(height: 280)
+          .clipShape(RoundedRectangle(cornerRadius: 24))
+          .overlay {
+            RoundedRectangle(cornerRadius: 24)
+              .stroke(.white.opacity(0.2), lineWidth: 1)
+          }
+          .shadow(radius: 10)
+          .padding(.horizontal)
           
           Spacer().frame(height: 60)
           
@@ -27,7 +48,9 @@ struct BarcodeScannerView: View {
             .font(.largeTitle)
             .foregroundStyle(viewModel.statusTextColor)
             .padding()
+            .animation(.spring(), value: viewModel.statusText)
         }
+        
         .navigationTitle("Barcode Scanner")
         .alert(item: $viewModel.alertItem) { alertItem in
           Alert(title: Text(alertItem.title),
@@ -36,8 +59,9 @@ struct BarcodeScannerView: View {
         }
       }
     }
+  }
 }
 
 #Preview {
-    BarcodeScannerView()
+  BarcodeScannerView()
 }
